b6b91a5b395e9e55439a17d2c2bcb086d8c432fd
Manuel Adolfo Soto <sotomanueladolfo1@gmail.com>
2026-05-31 01:39:00 -0400

    fix: actualizar service worker para forzar cache nuevo (v2) y estrategia network-first

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,20 @@
-const CACHE = 'khiomaru-cache-v1';
-const urlsToCache = ['/', '/index.html'];
+const CACHE = 'khiomaru-cache-v2';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) return response;
-      return fetch(event.request).then((fetchResponse) => {
-        if (fetchResponse && fetchResponse.status === 200) {
-          const clone = fetchResponse.clone();
-          caches.open(CACHE).then((cache) => cache.put(event.request, clone));
-        }
-        return fetchResponse;
-      });
-    })
+    fetch(event.request).then((response) => {
+      if (response && response.status === 200) {
+        const clone = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, clone));
+      }
+      return response;
+    }).catch(() =>
+      caches.match(event.request)
+    )
   );
 });
 
@@ -26,6 +22,6 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
